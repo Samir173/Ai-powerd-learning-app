e1683d8c3ba32442d68d23360835bb5812c502e9
Samir Memon <124080199+Samir173@users.noreply.github.com>
2026-09-02 17:04:11 -0400

    Update flashcard routes controller and server

---
+++ backend/controllers/flashcardController.js
@@ -0,0 +1,12 @@
+import Flashcard from "../models/Flashcard";
+
+// @desc get all flashcards for a document
+// @route GET /api/flashcards/:documentId
+// @access Private
+export const getFlashcards = async (req, res, next) => {
+  try {
+
+  } catch(error){
+    next(error);
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
-import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
-import connectDB from "./config/db.js";
-import errorHandler from "./middleware/errorHandle.js";
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import connectDB from './config/db.js';
+import errorHandler from './middleware/errorHandle.js';
 
-import authRoutes from "./routes/authRoutes.js";
-import documentRoutes from "./routes/documentRoutes.js"
+import authRoutes from './routes/authRoutes.js';
+import documentRoutes from './routes/documentRoutes.js';
+import flashcardRoutes from './routes/flashcardRoutes.js';
 
 
 //ES6 module__dirname alternative
@@ -36,11 +37,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Static folder for uploads
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use('/uploads', express.static(path.join(__dirname, "uploads")));
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/documents",documentRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/documents',documentRoutes);
+app.use('/api/flashcards',flashcardRoutes);
 
 app.use(errorHandler);
 
